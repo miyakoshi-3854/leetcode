858f47916799a0f1136b2db96bf58be07f521719
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-22 12:15:30 +0900

    feat(shell): add `init.sh`

---
+++ shell/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Colors
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
+echo -e "${GREEN}🔄 Initializing leetcode...${RESET}"
+
+cp _template/main.py main.py
+
+echo -e "${GREEN}✅ Done: leetcode initialized${RESET}"
