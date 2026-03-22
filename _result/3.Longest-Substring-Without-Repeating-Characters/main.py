3d490da55e0f0ddabd0f3937fd6334ea40307b52
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-22 15:01:54 +0900

    3.Longest-Substring-Without-Repeating-Characters

---
+++ _result/3.Longest-Substring-Without-Repeating-Characters/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+    # 最初の考え
+        # ノードを使って見るindexを変えていく
+        # 見たノードを一つ格納する
+        # 格納したノードを次のノードと一致するか検証する
+        # 検証した数をカウントする
+        # カウントした数を大きい場合に書き換える
+        # 結果を出力する        
+
+    # ソリューションを見た後の考え
+        # set変数を用意する
+        # result変数を用意する
+        # 左、右のポインタを用意する
+        # 見る範囲をひとつづつ右へずらしていく
+        # set変数が、右のポインタと一致したら、setの重複を消して、左のポインタをtrue分右に動かす
+        # これを文字列の最後までやって、result出力
+
+        charSet = set()
+        left = 0
+        result = 0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            
+            charSet.add(s[right])
+            result = max(result, right - left + 1)
+        
+        return result
