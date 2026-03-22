4778f5ab71baf036753fab531e016b8da56c13f6
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-22 15:04:39 +0900

    6.Zigzag-Conversion

---
+++ _result/6.Zigzag-Conversion/main.py
@@ -0,0 +1,25 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        # 斜め部分はnumRows - 2 の数になる
+        # 思いついたこと
+            # 例えば numRows が4なら行数は、1, 2, 3, 4, 3, 2, 1, ...繰り返しのようになる。
+            # これをストリングの長さ分のループで回す感じになるか？
+
+        if numRows == 1:
+            return s
+
+        rows = [""] * numRows
+        current_row = 0
+        direction = 0
+
+        for char in s:
+            rows[current_row] += char
+
+            if current_row == 0:
+                direction = 1
+            elif current_row == numRows - 1:
+                direction = - 1
+
+            current_row += direction
+        
+        return "".join(rows)
