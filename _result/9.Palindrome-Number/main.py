e2258ccb8cbe06b9466e8a12544c169a7903ff21
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-24 22:45:17 +0900

    9.Palindrome-Number

---
+++ _result/9.Palindrome-Number/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # xから一桁目を抜き取って、抜き取った順でnumに代入していく感じか？
+        # そしてx = numならtrue違うならfalseでいいか？
+        # エッジケースは、負の数の時で、早期リターンすればいいかな？ 
+
+        if x < 0:
+            return False
+
+        if x % 10 == 0 and x != 0:
+            return False
+
+        originalX = x
+        num = 0
+        while x != 0:
+            digit = x % 10
+            num = num * 10 + digit
+            x //= 10
+
+        return originalX == num
