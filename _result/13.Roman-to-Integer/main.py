5d7f3a08d767864a8a61ba13f46ef4badf68f302
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-28 17:55:42 +0900

    13.Roman-to-Integer

---
+++ _result/13.Roman-to-Integer/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_value = {
+            "M": 1000,
+            "CM": 900,
+            "D":  500,
+            "CD": 400,
+            "C":  100,
+            "XC":  90,
+            "L":   50,
+            "XL":  40,
+            "X":   10,
+            "IX":   9,
+            "V":    5,
+            "IV":   4,
+            "I":    1
+        }
+
+        num = 0
+
+        idx = 0
+        while idx < len(s):
+            two_symbol = s[idx : idx + 2]
+            if two_symbol in roman_value:
+                num += roman_value[two_symbol]
+                idx += 2
+            elif s[idx] in roman_value:
+                num += roman_value[s[idx]]
+                idx += 1
+
+        return num
