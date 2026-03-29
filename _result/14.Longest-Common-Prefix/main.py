64b12c032650cf2b74e6f32f343ffde87ceafbc6
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-29 16:24:59 +0900

    14.Longest-Common-Prefix

---
+++ _result/14.Longest-Common-Prefix/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        for i in range(len(min(strs, key = len))):
+            all_chars_match = [s[i] == strs[0][i] for s in strs]
+            if all(all_chars_match):
+                prefix += strs[0][i]
+            else:
+                return prefix
+
+        return prefix
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.solve()
