cb2cd0f03ff21987bd991a228057387286caf21a
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-30 23:10:01 +0900

    0015 (main_2.py)

---
+++ _result/0015/main_2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        result_lists = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    result_lists.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+        return result_lists
