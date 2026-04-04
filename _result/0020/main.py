9843061d839511d5cd34d2fb7189def6ed50d9f5
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-04 11:35:36 +0900

    0020 (main.py)

---
+++ _result/0020/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_bracket = (
+            "(",
+            "[",
+            "{",
+        )
+        close_bracket = (
+            ")",
+            "]",
+            "}",
+        )
+
+        if len(s) % 2 == 1:
+            return False
+
+        stack = []
+        for char in s:
+            if char in open_bracket:
+                stack.append(char)
+                continue
+
+            if not stack and char in close_bracket:
+                return False
+            elif open_bracket.index(stack[- 1]) == close_bracket.index(char):
+                stack.pop()
+                continue
+            else:
+                return False
+
+        return not stack
