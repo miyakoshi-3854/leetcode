4a93d842f9d028c00ac9f83b61e3564fc532e5eb
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-09 23:26:28 +0900

    0025 (main_2.py)

---
+++ _result/0025/main_2.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        if k == 1:
+            return head
+
+        stack = []
+        while head:
+            next_node = head.next
+            head.next = None
+            stack.append(head)
+            head = next_node
+            
+            if len(stack) == k:
+                for _ in range(k):
+                    tail.next = stack.pop()
+                    tail = tail.next
+                stack.clear()
+
+        for node in stack:
+            tail.next = node
+            tail = tail.next
+
+        return dummy.next
