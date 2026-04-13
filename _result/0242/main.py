d245f2c40318904e9671cb7d486f80fc5b1622a5
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-13 22:02:00 +0900

    0242 (main.py)

---
+++ _result/0242/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        char_count = defaultdict(int)
+
+        for char in s:
+            char_count[char] += 1
+
+        for char in t:
+            if char not in char_count:
+                return False
+            else:
+                char_count[char] -= 1
+
+        for value in char_count.values():
+            if value != 0:
+                return False
+        
+        return True
