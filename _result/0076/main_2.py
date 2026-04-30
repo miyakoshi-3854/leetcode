0fb8cb6934d593625d37885fa7f8709d6ba3bcb0
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-30 22:54:45 +0900

    0076 (main_2.py)

---
+++ _result/0076/main_2.py
@@ -0,0 +1,46 @@
+"""
+76. Minimum Window Substring
+
+条件
+tのanaglam + αが存在する最小のwindowを返す
+
+思考
+長さが不定の尺取り法
+びびらずfor rが右へ舐める
+
+まず成り立つ条件は、
+- tのanaglam + αがwindowに存在する
+
+どんな条件のときに、lを詰めればいいか考える必要がある
+→ window内のs[l]の個数 > tで必要なs[l]の個数のとき動かせる
+
+window長さを最小にする
+min(len(cur_window), len(window))
+
+"""
+
+from collections import Counter, defaultdict
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        t_d = Counter(t)
+        required = len(t_d)
+
+        window_d = defaultdict(int)
+        formed = 0
+        min_window = ""
+        l = 0
+        for r in range(len(s)):
+            window_d[s[r]] += 1
+            if s[r] in t_d and window_d[s[r]] == t_d[s[r]]:
+                formed += 1
+
+            while formed == required and window_d[s[l]] > t_d[s[l]]:
+                window_d[s[l]] -= 1
+                l += 1
+
+            if formed == required and (min_window == "" or r - l + 1 < len(min_window)):
+                min_window = s[l : r + 1]
+
+        return min_window
