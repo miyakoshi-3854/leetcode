0868979e861b677a437a468ca351893e5a2d6082
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-05-13 10:39:28 +0900

    0033 (main.py)

---
+++ _result/0033/main.py
@@ -0,0 +1,32 @@
+"""
+33. Search in Rotated Sorted Array
+
+条件
+targetのindexを返す
+ないなら-1
+
+思考
+153とめっちゃ似てる
+
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[l] <= nums[m]:
+                if nums[l] <= target <= nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] <= target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+
+        return -1
